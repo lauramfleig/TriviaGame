b574bda3d403a3e04ad6cdd6b052425cb4f6b84f
Laura Fleig <laurafleig@Lauras-MacBook-Pro.local>
2017-12-08 21:20:33 -0800

    homeowrk

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,52 +1,72 @@
 //Global Variables
 
-var seconds = 5;
+var seconds = 10;
 var newSeconds;
 var correctAnswers = 0;
 var incorrectAnswers = 0;
-var time0 = false;
-var counting = true
+var unanswered = 3;
 
 //Functions
 
 //Subtract one from seconds
 function count() {
-	seconds = seconds - 1;
-	//Get Time display on #timer
-	$("#time").text('Time Remaining: ' + seconds)
+	if (seconds > 0) {
+		seconds = seconds - 1;
+		//Get Time display on #timer
+		$("#time").text('Time Remaining: ' + seconds)
+
+	} else {
+		$('.Game-Screen-2').hide()
+		$('.Final-Screen').show()
+	}
 }
 //Subtract one from seconds every second
 function countDown() {
-	var newSeconds = setInterval(count, 1000);
+	 newSeconds = setInterval(count, 1000);
 }
 
 //Add one to correct answers
 function addOneC() {
 	correctAnswers = correctAnswers + 1;
-
+	$('.final-correct').text('Correct Answers: ' + correctAnswers);
+	
 }
 
+//Add one to incorrect answers
 function addOneI() {
 	incorrectAnswers = incorrectAnswers + 1;
+	$('.final-incorrect').text('Incorrect Answers: ' + incorrectAnswers);
 }
 
-function stopAtZero() {
-	counting = false
+function subtractUA() {
+	
+	unanswered = unanswered - 1;
+	$('.unanswered').text('Unanswered: ' + unanswered)
 }
+
+function originalState() {
+	seconds = 10;
+	correctAnswers = 0;
+	incorrectAnswers = 0;
+	unanswered = 3;
+	//Hide game screens 2 and final
+	$('.Game-Screen-2').hide()
+	$('.Final-Screen').hide()
+	//Show game screen 1
+	$('.Game-Screen').show()
+
+}
+
 
 //When the page loads
 $( document ).ready() 
 
-	//Show the game-screen div
-	$(".Game-Screen").show()
-	//Hide the game-screen-2 div
-	$(".Game-Screen-2").hide()
+	originalState()
 
 	
 	//When the Start Button is Clicked
 	$(".start-button").on('click', function(){
 
-		
 		//Hide the game-screen div
 		$(".Game-Screen").hide()
 		//Show the game-screen-2 div
@@ -54,27 +74,69 @@ $( document ).ready()
 
 		countDown()
 
+		//When any answer in the first question is clicked, disable the others
+		$('.answer').on('click', function() {
+			$('.answer').attr('disabled', 'disabled');
+		})
+
+		//When any answer in the second question is clicked, disable the others
+		$('.thing').on('click', function() {
+			$('.thing').attr('disabled', 'disabled')
+		})
+
+		//When any answer in the third question is clicked, disable the others
+		$('.boop').on('click', function() {
+			$('.boop').attr('disabled', 'disabled')
+			$('boop').setAttribute('checked')
+		})
+		
 		//When a correct choice circle is clicked
-		$(".correct").on('click', function(){
-		addOneC()
+		$('.correct').on('click', function(){
+			addOneC()
+			subtractUA()
 		})
 
 		//When an incorrect choice circle is clicked
-		$(".incorrect").on('click', function(){
-		addOneI()
+		$('.incorrect').on('click', function(){
+			addOneI()
+			subtractUA()
 		})
+
+		//If answers have not been clicked, display 0 on the final screen
+		$('.final-correct').text('Correct Answers: ' + correctAnswers)
+		$('.final-incorrect').text('Incorrect Answers: ' + incorrectAnswers)
+		$('.unanswered').text('Unanswered: ' + unanswered)
+			
+
+	})
+
+	//When the submit button is clicked
+	$(".submit").on('click', function(){
+
+		//Hide game-screen-2 div
+		$('.Game-Screen-2').hide()
+		//Show final-screen
+		$('.Final-Screen').show()
+
+	})
+
+	//When the reset button is clicked
+	$(".reset").on('click', function(){
+		//Call Function to set game to original state
+		originalState()
+
+		clearInterval(newSeconds)
+		$('#time').text('Time Remaining: ' + seconds)
+
+
+
+		
+
+
 
 	})
 
 
 
 
-
-
-
-//When the times gets to zero
-	//If there are any unclicked circles, 1 is added to unanswered
-	//game-text.div disappears
-	//game-end-text appears
-		//displays correct, incorrect, and unanswered variables 
 
